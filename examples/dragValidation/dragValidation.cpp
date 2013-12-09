a3925de23309d0921999ac6a66435377197be062
Philippe Seil <philippe.seil@jku.at>
2013-12-09 10:08:52 +0100

    Finished example for drag validation. Drag is not exactly correct, will have to check this in the future

--- examples/dragValidation/dragValidation.cpp
+++ examples/dragValidation/dragValidation.cpp
@@ -20,39 +20,64 @@ typedef double T;
 
 const T pi = 4.*atan(1.);
 void writeVTK(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,
-              IncomprFlowParam<T> const& parameters, plint iter)
+              IncomprFlowParam<T> const& parameters,
+              PhysUnits3D<T> const& units, plint iter)
 {
-    T dx = parameters.getDeltaX();
-    T dt = parameters.getDeltaT();
-    std::string fname(createFileName("vtk", iter, 6));
-    VtkImageOutput3D<T> vtkOut(fname, dx);
-    vtkOut.writeData<float>(*computeVelocityNorm(lattice), "velocityNorm", dx/dt);
-    vtkOut.writeData<3,float>(*computeVelocity(lattice), "velocity", dx/dt);   
-
-    MultiScalarField3D<T> tmp(lattice.getNx(),lattice.getNy(),lattice.getNz());
-
-    applyProcessingFunctional
-      (new GetExternalScalarFunctional3D<T,DESCRIPTOR,T>(DESCRIPTOR<T>::ExternalField::volumeFractionBeginsAt),
-       lattice.getBoundingBox(), lattice, tmp);
-    vtkOut.writeData<float>(tmp, "SolidFraction", 1.);
-
-    pcout << "wrote " << fname << std::endl;
+  
+  MultiScalarField3D<T> tmp(lattice.getNx(),lattice.getNy(),lattice.getNz());
+  
+  T p_fact = units.getPhysForce(1)/pow(units.getPhysLength(1),2)/3.;
+  
+  std::string fname(createFileName("vtk", iter, 6));
+  
+  VtkImageOutput3D<T> vtkOut(fname, units.getPhysLength(1));
+  vtkOut.writeData<float>(*computeVelocityNorm(lattice), "velocityNorm", units.getPhysVel(1));
+  vtkOut.writeData<3,float>(*computeVelocity(lattice), "velocity", units.getPhysVel(1));  
+  vtkOut.writeData<float>(*computeDensity(lattice), "density",units.getPhysDensity(1)); 
+  
+  MultiScalarField3D<T> p(*computeDensity(lattice));
+  subtractInPlace(p,1.);
+  vtkOut.writeData<float>(p,"pressure",p_fact ); 
+  
+  
+  applyProcessingFunctional
+    (new GetExternalScalarFunctional3D<T,DESCRIPTOR,T>(DESCRIPTOR<T>::ExternalField::volumeFractionBeginsAt),
+     lattice.getBoundingBox(), lattice, tmp);
+  vtkOut.writeData<float>(tmp, "SolidFraction", 1.);
+  
+  pcout << "wrote " << fname << std::endl;
 }
 
-void writeGif(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,int iT)
+
+void writeToAscii(std::string fnamePrefix, MultiScalarField3D<T> &data, int iT)
 {
-    const plint imSize = 600;
-    const plint nx = lattice.getNx();
-    const plint ny = lattice.getNy();
-    const plint nz = lattice.getNz();
-    Box3D slice(0, nx-1, (ny-1)/2, (ny-1)/2, 0, nz-1);
-    //Box3D slice(0, nx-1, 0, ny-1, (nz-1)/2, (nz-1)/2);
-    ImageWriter<T> imageWriter("leeloo.map");
-    std::string fname(createFileName("u", iT, 6));
-    imageWriter.writeScaledGif(fname,
-                               *computeVelocityNorm(lattice, slice),
-                               imSize, imSize);
-    pcout << "wrote " << fname << std::endl;
+  std::string fname(global::directories().getOutputDir());
+  fname.append(createFileName(fnamePrefix, iT, 6));
+  fname.append(".txt");
+  plb_ofstream ofile(fname.c_str());
+  ofile << setprecision(10) << data << endl;
+  ofile.close();
+
+}
+
+
+void writeAscii(MultiBlockLattice3D<T,DESCRIPTOR>& lattice,int iT)
+{
+  
+  MultiScalarField3D<T> tmp(lattice.getNx(),lattice.getNy(),lattice.getNz());
+                            
+  applyProcessingFunctional
+    (new GetExternalScalarFunctional3D<T,DESCRIPTOR,T>(DESCRIPTOR<T>::ExternalField::hydrodynamicForceBeginsAt),
+     lattice.getBoundingBox(), lattice, tmp);
+  
+  writeToAscii("fx",tmp,iT);
+  
+  applyProcessingFunctional
+    (new GetExternalScalarFunctional3D<T,DESCRIPTOR,T>(DESCRIPTOR<T>::ExternalField::volumeFractionBeginsAt),
+     lattice.getBoundingBox(), lattice, tmp);
+  
+  writeToAscii("fs",tmp,iT);
+  writeToAscii("ux",*computeVelocityComponent(lattice,lattice.getBoundingBox(),0),iT);
 }
 
 int main(int argc, char* argv[]) {
@@ -60,87 +85,101 @@ int main(int argc, char* argv[]) {
     plbInit(&argc, &argv);
     global::directories().setOutputDir("./tmp/");
 
-    if (argc != 3) {
+    if (argc != 6) {
         pcout << "Error the parameters are wrong. The structure must be :\n";
         pcout << "1 : N\n";
-        pcout << "2 : rho_s/rho_f\n";
+        pcout << "2 : u0\n";
+        pcout << "3 : uMax\n";
+        pcout << "4 : maxT\n";
+        pcout << "5 : vtkT\n";
         exit(1);
     }
 
     const plint N = atoi(argv[1]);
-    const T rho_fact = atof(argv[2]);
-    const T uMax = 0.02;
+    const T u0 = atof(argv[2]);
+    const T uMax = atof(argv[3]);
+    const T maxT = atof(argv[4]);
+    const T vtkT = atof(argv[5]);
 
     const T rho_f = 1000;
+
+    const T lx = 0.6, ly = 0.2, lz = 0.2;
 
     plint *id = new plint;
     *id = 0;
     T **x, **v, **f, **t, **omega;
-    T r = 0.01;
-    T rho_s = rho_f*rho_fact;
-    T m = 4./3.*r*r*r*3.14*rho_s;
-    T I = 2./5.*m*r*r;
+    T r = 0.02;
     x = new T*[1]; x[0] = new T[3];
     v = new T*[1]; v[0] = new T[3];
     omega = new T*[1]; omega[0] = new T[3];
-
-    T g = 9.81;
-
-    pcout << "m: " << m << " | I: " << I << std::endl;
-
-    const T lx = 0.2, ly = 0.2, lz = 0.4;
-
-    x[0][0] = lx/2; x[0][1] = ly/2; x[0][2] = lz-2*r;
-    v[0][0] = 0; v[0][1] = 0; v[0][2] = 0;
-    omega[0][0] = 0; omega[0][1] = 0; omega[0][2] = 0;
-
-    T v_inf = sqrt(2.*m*g/(rho_f*r*r*3.14*0.47));
-
-    pcout << "v_inf: " << v_inf << std::endl;
-
-
-    const T nu_f = 1e-4;
-    //    PhysUnits3D<T> units(lx,v_inf,nu_f,lx,ly,lz,N,uMax,rho_f);
-    PhysUnits3D<T> units(2.*r,v_inf,nu_f,lx,ly,lz,N,uMax,rho_f);
-
-    IncomprFlowParam<T> parameters(units.getLbParam());
-
-    
-    const T maxT = (T)10.0;
-    const T vtkT = 0.05;
-    const T gifT = 100;
-    const T logT = 0.01;
-
-    const plint maxSteps = units.getLbSteps(maxT);
-    const plint vtkSteps = max<plint>(units.getLbSteps(vtkT),1);
-    const plint gifSteps = units.getLbSteps(gifT);
-    const plint logSteps = max<plint>(units.getLbSteps(logT),1);
-
-    writeLogFile(parameters, "3D sedimenting sphere");
-
-    MultiBlockLattice3D<T, DESCRIPTOR> lattice (
-        parameters.getNx(), parameters.getNy(), parameters.getNz(), 
-        new DYNAMICS );
-
-    // Use periodic boundary conditions.
-    lattice.periodicity().toggle(2,true);
-
-    OnLatticeBoundaryCondition3D<T,DESCRIPTOR>* boundaryCondition
-        = createLocalBoundaryCondition3D<T,DESCRIPTOR>();
-
-
     f = new T*[1]; f[0] = new T[3];
     t = new T*[1]; t[0] = new T[3];
+
+
+    x[0][0] = 0.2; x[0][1] = 0.1; x[0][2] = 0.1;
+    v[0][0] = 0; v[0][1] = 0; v[0][2] = 0;
+    omega[0][0] = 0; omega[0][1] = 0; omega[0][2] = 0;
     
     f[0][0] = 0; f[0][1] = 0; f[0][2] = 0;
     t[0][0] = 0; t[0][1] = 0; t[0][2] = 0;
 
 
-    T dt_dem = units.getPhysTime(1);
-    pcout << "omega: " << parameters.getOmega() << "\n" 
-          << "dt_dem: " << dt_dem << "\n"
-          << "Re : " << parameters.getRe() << std::endl;
+    const T nu_f = 1e-4;
+
+    PhysUnits3D<T> units(2.*r,u0,nu_f,lx,ly,lz,N,uMax,rho_f);
+
+    IncomprFlowParam<T> parameters(units.getLbParam());
+
     
+    const T logT = 0.05;
+
+    const plint maxSteps = units.getLbSteps(maxT);
+    const plint vtkSteps = max<plint>(units.getLbSteps(vtkT),1);
+    const plint logSteps = max<plint>(units.getLbSteps(logT),1);
+
+    writeLogFile(parameters, "3D sedimenting sphere");
+
+    const plint nx = parameters.getNx(), ny = parameters.getNy(), nz = parameters.getNz();
+
+    MultiBlockLattice3D<T, DESCRIPTOR> lattice (nx,ny,nz,new DYNAMICS);
+
+    plint envelopeWidth = 1;
+
+    lattice.periodicity().toggleAll(false);
+    lattice.periodicity().toggle(1,true);
+    lattice.periodicity().toggle(2,true);
+
+    OnLatticeBoundaryCondition3D<T,DESCRIPTOR>* boundaryCondition
+      = createLocalBoundaryCondition3D<T,DESCRIPTOR>();
+
+
+    Box3D interior(1,nx-2,1,ny-2,1,nz-2);
+    Box3D inlet(0,0,0,ny-1,0,nz-1),
+      outlet(nx-1,nx-1,0,ny-1,0,nz-1);
+    
+    Array<T,3> u0_vec(uMax,0.,0.);
+
+    boundaryCondition->setVelocityConditionOnBlockBoundaries ( lattice, inlet );
+    boundaryCondition->setPressureConditionOnBlockBoundaries (lattice, outlet );
+    
+    setBoundaryDensity(lattice,outlet,1.);
+    setBoundaryVelocity(lattice, inlet, u0_vec );
+    
+    initializeAtEquilibrium(lattice,lattice.getBoundingBox(),1.,u0_vec);
+
+    lattice.periodicity().toggleAll(false);
+    lattice.periodicity().toggle(1,true);
+    lattice.periodicity().toggle(2,true);
+
+    T Re = parameters.getRe();
+
+    pcout << "omega: " << parameters.getOmega() << "\n" 
+          << "Re : " << Re << "\n"
+          << "grid size: " << nx << " " << ny << " " << nz << "\n"
+          << "physical time step: " << units.getPhysTime(1) << std::endl;
+
+    T c_drag_sn = 24./Re*(1. + 0.15*pow(Re,0.687));
+
     T **x_lb, **v_lb, **omega_lb;
     x_lb = new T*[1]; x_lb[0] = new T[3];
     v_lb = new T*[1]; v_lb[0] = new T[3];
@@ -156,63 +195,35 @@ int main(int argc, char* argv[]) {
     applyProcessingFunctional
       (new SetSpheres3D<T,DESCRIPTOR>(x_lb,v_lb,omega_lb,&r_lb,id,1),
        lattice.getBoundingBox(), lattice);
-    
-
-
-
+    pcout << "spheres set" << std::endl;
     clock_t start = clock();    
 
     // Loop over main time iteration.
-    //    for (plint iT=0; iT<parameters.nStep(maxT); ++iT) {
-    pcout << units.getLbSteps(0.01) << std::endl;
     for (plint iT=0; iT<maxSteps; ++iT) {
-
-      v[0][0] += 0.5*dt_dem*f[0][0]/m;
-      v[0][1] += 0.5*dt_dem*f[0][1]/m;
-      v[0][2] += 0.5*dt_dem*(f[0][2]/m-g*(rho_fact-1));
-
-      x[0][0] += v[0][0]*dt_dem + 0.5*f[0][0]*dt_dem*dt_dem/m;
-      x[0][1] += v[0][1]*dt_dem + 0.5*f[0][1]*dt_dem*dt_dem/m;
-      x[0][2] += v[0][2]*dt_dem + 0.5*dt_dem*dt_dem*(f[0][2]/m-g*(rho_fact-1));
-
-      omega[0][0] += 0.5*dt_dem*t[0][0]/I;
-      omega[0][1] += 0.5*dt_dem*t[0][1]/I;
-      omega[0][2] += 0.5*dt_dem*t[0][2]/I;
-
-      //      if(iT%parameters.nStep(vtkT) == 0)
-      if(iT%vtkSteps == 0)
-        writeVTK(lattice,parameters,iT);
-
-      //      if(iT%parameters.nStep(gifT) == 0)
-      if(iT%gifSteps == 0)
-        writeGif(lattice,iT);
-      
       for(int i=0;i<3;i++){
         f[0][i] = 0; t[0][i] = 0;
       }
       
-      
-      for(plint i=0;i<3;i++){
-        x_lb[0][i] = units.getLbLength(x[0][i]);
-        v_lb[0][i] = units.getLbVel(v[0][i]);
-        omega_lb[0][i] = units.getLbFreq(omega[0][i]);
-      }
-      
-      applyProcessingFunctional
-        (new SetSpheres3D<T,DESCRIPTOR>(x_lb,v_lb,omega_lb,&r_lb,id,1),
-         lattice.getBoundingBox(), lattice);
-
       lattice.collideAndStream();
-      
-      
-      if(iT > 1) 
-        applyProcessingFunctional(new SumForceTorque3D<T,DESCRIPTOR>(id,x_lb,f,t),
-                                  lattice.getBoundingBox(), lattice);
-      for(int i=0;i<3;i++){
-        f[0][i] = units.getPhysForce(f[0][i]);
-        t[0][i] = units.getPhysTorque(t[0][i]);
-      }
 
+      if(iT%vtkSteps == 0)
+        writeVTK(lattice,parameters,units,iT);
+
+      SumForceTorque3D<T,DESCRIPTOR> funct(1,x);
+      if(iT > 1) 
+        applyProcessingFunctional(funct, lattice.getBoundingBox(), lattice);
+      
+      
+      std::vector<double> &v_ft = funct.getForceTorque();
+      
+      for(int i=0;i<3;i++){
+        f[0][i] = units.getPhysForce(v_ft[i]);
+        t[0][i] = units.getPhysTorque(v_ft[i+3]);
+      }
+      
+
+      T c_drag = 2.*f[0][0]/(rho_f*u0*u0*r*r*3.1416);
+      
       if(iT%logSteps == 0){
         clock_t end = clock();
         T time = difftime(end,start)/((T)CLOCKS_PER_SEC);
@@ -220,23 +231,13 @@ int main(int argc, char* argv[]) {
         pcout << "time: " << time << " " ;
         pcout << "calculating at " << mlups << " MLU/s" << std::endl;
         start = clock();
-        pcerr << x[0][0] << " " << x[0][1] << " " << x[0][2] << " | ";
-        pcerr << v[0][0] << " " << v[0][1] << " " << v[0][2] << " | ";
-        pcerr << omega[0][0] << " " << omega[0][1] << " " << omega[0][2] << " | ";
+        pcerr << units.getPhysTime((T)iT) << " | ";
         pcerr << f[0][0] << " " << f[0][1] << " " << f[0][2] << " | ";
-        pcerr << t[0][0] << " " << t[0][1] << " " << t[0][2] << std::endl;
+        pcerr << t[0][0] << " " << t[0][1] << " " << t[0][2] << " | ";
+        pcerr << c_drag << " " << c_drag_sn << " | ";
+        pcerr << sqrt(2.*f[0][0]/(rho_f*u0*u0*3.1416*c_drag_sn)) << std::endl;
       }
       
-
-
-      v[0][0] += 0.5*dt_dem*f[0][0]/m;
-      v[0][1] += 0.5*dt_dem*f[0][1]/m;
-      v[0][2] += 0.5*dt_dem*(f[0][2]/m-g*(rho_fact-1));
-
-      omega[0][0] += 0.5*dt_dem*t[0][0]/I;
-      omega[0][1] += 0.5*dt_dem*t[0][1]/I;
-      omega[0][2] += 0.5*dt_dem*t[0][2]/I;
-
     }
 
     delete boundaryCondition;
