69f69b65644f7a54eb396ae02218689dd01819e4
Philippe Seil <philippe.seil@jku.at>
2016-03-08 09:51:03 +0100

    added possibility to set offset for unit conversion to physunits.h

--- src/physunits.h
+++ src/physunits.h
@@ -32,14 +32,26 @@ namespace plb {
     PhysUnits3D(T const l_p_, T const u_p_, T const nu_p_, T const lx_, T const ly_, T const lz_,
                 plint const N, T const uMax, T const rho_p_)
       : l_p(l_p_), u_p(u_p_), t_p(l_p_/u_p_), nu_p(nu_p_), lx(lx_), ly(ly_), lz(lz_), rho_p(rho_p_),
-      Re(l_p_*u_p_/nu_p_), param(uMax,Re,N,lx_/l_p_,ly_/l_p_,lz/l_p_),
-      forceFactor(pow(param.getDeltaX(),4)/pow(param.getDeltaT(),2) * rho_p * pow(l_p,4)/pow(t_p,2)),
-      torqueFactor(pow(param.getDeltaX(),5)/pow(param.getDeltaT(),2) * rho_p * pow(l_p,5)/pow(t_p,2)) {}
+        Re(l_p_*u_p_/nu_p_), param(uMax,Re,N,lx_/l_p_,ly_/l_p_,lz/l_p_),
+        forceFactor(pow(param.getDeltaX(),4)/pow(param.getDeltaT(),2) * rho_p * pow(l_p,4)/pow(t_p,2)),
+        torqueFactor(pow(param.getDeltaX(),5)/pow(param.getDeltaT(),2) * rho_p * pow(l_p,5)/pow(t_p,2)),
+        lbOffsetX(0.5), lbOffsetY(0.5), lbOffsetZ(0.5)
+    {}
 
     T getRe() const {return Re; };
 
+    void setLbOffset(T const lbOffsetX_,T const lbOffsetY_,T const lbOffsetZ_)
+    {
+      lbOffsetX = lbOffsetX_;
+      lbOffsetY = lbOffsetY_;
+      lbOffsetZ = lbOffsetZ_;
+    }
+
     T getLbLength(T const physL) const { return physL / l_p / param.getDeltaX() ; };
-    T getLbPosition(T const physL) const { return physL / l_p / param.getDeltaX() - 0.5 ; };
+    T getLbPosition(T const physL) const { return physL / l_p / param.getDeltaX() - 0.5; };
+    T getLbPositionX(T const physL) const { return physL / l_p / param.getDeltaX() - lbOffsetX ; };
+    T getLbPositionY(T const physL) const { return physL / l_p / param.getDeltaX() - lbOffsetY ; };
+    T getLbPositionZ(T const physL) const { return physL / l_p / param.getDeltaX() - lbOffsetZ ; };
     T getLbVel(T const physVel) const { return physVel / u_p * param.getLatticeU(); };
     T getLbTime(T const physTime) const { return physTime / t_p / param.getDeltaT(); };
     T getLbDensity(T const physDensity) const { return physDensity / rho_p; };
@@ -52,6 +64,9 @@ namespace plb {
     
     T getPhysLength(T const lbLength) const { return lbLength*param.getDeltaX()*l_p; };
     T getPhysPosition(T const lbLength) const { return (lbLength+0.5)*param.getDeltaX()*l_p; };
+    T getPhysPositionX(T const lbLength) const { return (lbLength+lbOffsetX)*param.getDeltaX()*l_p; };
+    T getPhysPositionY(T const lbLength) const { return (lbLength+lbOffsetY)*param.getDeltaX()*l_p; };
+    T getPhysPositionZ(T const lbLength) const { return (lbLength+lbOffsetZ)*param.getDeltaX()*l_p; };
     T getPhysVel(T const lbVel) const { return lbVel/param.getLatticeU()*u_p; };
     T getPhysTime(T const lbTime) const { return lbTime*param.getDeltaT()*t_p; };
     T getPhysDensity(T const lbDensity) const { return lbDensity * rho_p; };
@@ -73,6 +88,7 @@ namespace plb {
     T const Re;
     IncomprFlowParam<T> param;
     T const forceFactor, torqueFactor;
+    T lbOffsetX, lbOffsetY, lbOffsetZ;
   };
 
 };
